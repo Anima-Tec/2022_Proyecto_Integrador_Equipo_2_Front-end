a8af6b88033f8b2c7dc96fc786d4b5e112ab6815
Michael Flores <mich4el.flores@gmail.com>
2022-08-20 16:42:56 -0300

    Install x.styled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, Preflight, x } from '@xstyled/styled-components';
+import theme from './config/theme';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Preflight />
+      <x.div>Index</x.div>
+    </ThemeProvider>
   );
 }
-
-export default App;
---
+++ src/config/theme.js
@@ -0,0 +1,8 @@
+import { defaultTheme } from '@xstyled/styled-components';
+
+const theme = {
+  ...defaultTheme
+  // Customize your theme here
+};
+
+export default theme;
