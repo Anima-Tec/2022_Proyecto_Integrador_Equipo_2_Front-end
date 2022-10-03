df709ba376834da97c0e62910393371c9ea52fe6
lucia-sebe <79849648+lucia-sebe@users.noreply.github.com>
2022-10-03 18:49:13 -0300

    Create view center component (#5)
    * Create view center component
    
    * Adding suggestions

---
+++ src/components/CenterCard.js
@@ -0,0 +1,39 @@
+import { x } from '@xstyled/styled-components';
+import P from 'componetes/font-styles/P';
+
+export default function CenterCard() {
+  return (
+    <x.div
+      w="100%"
+      p={{ _: '17px', lg: '23px' }}
+      display="flex"
+      boxShadow="0px 4px 20px rgba(0, 0, 0, 0.1)"
+      borderRadius="20px"
+      gap="26px"
+    >
+      <x.img
+        src="/image.type"
+        w={{ _: '112px', lg: '203px' }}
+        h={{ _: '90px', lg: '153px' }}
+        borderRadius="20px"
+        objectFit="cover"
+      />
+      <x.div
+        w="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        fontSize={{ _: '12px', lg: '16px' }}
+      >
+        <P fontSize={{ _: '12px', lg: '16px' }} fontWeight="bold">
+          Nombre
+        </P>
+        <P fontSize={{ _: '12px', lg: '16px' }}>Se requieren alimentos </P>
+        <P fontSize={{ _: '12px', lg: '16px' }}>Se requieren voluntarios </P>
+      </x.div>
+      <P fontSize={{ _: '12px', lg: '16px' }} textAlign="end">
+        Toca para más detalles
+      </P>
+    </x.div>
+  );
+}
