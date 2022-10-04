dbdfdcfda15b48a8960a9798e1d073983a23a5b8
Michael Flores <mich4el.flores@gmail.com>
2022-10-03 22:56:30 -0300

    Create Onboarding page (WIP) Create text screen Create StepCounter component

---
+++ src/components/onboarding/screens/text-screen.js
@@ -0,0 +1,21 @@
+import { x } from '@xstyled/styled-components';
+import { Logo } from 'components/Logo';
+import PropTypes from 'prop-types';
+import { StepCounter } from 'components/StepCounter';
+
+export const TextScreen = ({ children, step }) => {
+  return (
+    <>
+      {step && <StepCounter step={step} />}
+      <x.div display="flex" flexDirection="column" alignItems="center">
+        <Logo width="303px" height="303px" mb="55px" />
+        {children}
+      </x.div>
+    </>
+  );
+};
+
+TextScreen.propTypes = {
+  children: PropTypes.node.isRequired,
+  step: PropTypes.number
+};
