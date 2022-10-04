a8183055f9d0494c11228ac4ae604ebea74f9ec0
Michael Flores <mich4el.flores@gmail.com>
2022-10-03 23:04:28 -0300

    Create Center Information Screen - Create ContactButton component - Create NeedFoodList component - Add ilustration images

---
+++ src/components/centers/NeedFoodsList.js
@@ -0,0 +1,38 @@
+import { x } from '@xstyled/styled-components';
+import { P } from 'components/font-styles';
+import PropTypes from 'prop-types';
+
+const NeedFoodList = ({ foods }) => {
+  return (
+    <x.div w="100%">
+      <P fontWeight="bold" mb="15px">
+        Se requieren los siguientes alimentos:
+      </P>
+      <x.ul
+        listStyleType="disc"
+        listStylePosition="inside"
+        h="10rem"
+        overflow="scroll"
+      >
+        {foods.map((food, index) => (
+          <x.li key={index} mb={index !== foods.length - 1 && '15px'}>
+            {food.amount} {food.unitMeasurement} de {food.name}
+          </x.li>
+        ))}
+      </x.ul>
+    </x.div>
+  );
+};
+
+NeedFoodList.propTypes = {
+  foods: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      amount: PropTypes.number.isRequired,
+      unitMeasurement: PropTypes.oneOf(['KG', 'BAG', 'G', 'ML', 'L'])
+    })
+  ).isRequired
+};
+
+export { NeedFoodList };
