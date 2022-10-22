fe1f041d18d60347c722da19848b253d88896449
Micha <mich4el.flores@gmail.com>
2022-10-22 00:39:22 -0300

    :ambulance: Add Suspense and Lazy in authenticated routes

--- src/routers/authenticated/index.js
+++ src/routers/authenticated/index.js
@@ -1,8 +1,8 @@
-/* eslint-disable react/prop-types */
-import { lazy } from 'react'
+import { lazy, Suspense } from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { useLogout } from 'hooks/auth/mutations/useLogout'
 import { useCurrentUser } from 'hooks/users/queries/getCurrentUser'
+import { FullScreenLoader } from 'components/Loader'
 
 const Dashboard = lazy(() => import('pages/Dashboard'))
 const Onboarding = lazy(() => import('pages/Onboarding'))
@@ -12,31 +12,33 @@ export function Authenticated() {
   const { isLoading, data: user } = useCurrentUser()
 
   return (
-    !isLoading && (
-      <Routes>
-        {user?.onboardingStepPosition !== -1 ? (
-          <>
-            <Route
-              exact
-              path="/"
-              element={<Navigate to={'/onboarding'} replace={true} />}
-            />
-            <Route exact path="/onboarding" element={<Onboarding />} />
-          </>
-        ) : (
-          <>
-            <Route
-              exact
-              path="/"
-              element={<Navigate to={'/dashboard'} replace={true} />}
-            />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route exact path="/auth/logout" element={<Logout />} />
-          </>
-        )}
-        <Route exact path="*" element={<Page404 />} />
-      </Routes>
-    )
+    <Suspense fallback={<FullScreenLoader />}>
+      {!isLoading && (
+        <Routes>
+          {user?.onboardingStepPosition !== -1 ? (
+            <>
+              <Route
+                exact
+                path="/"
+                element={<Navigate to={'/onboarding'} replace={true} />}
+              />
+              <Route exact path="/onboarding" element={<Onboarding />} />
+            </>
+          ) : (
+            <>
+              <Route
+                exact
+                path="/"
+                element={<Navigate to={'/dashboard'} replace={true} />}
+              />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/auth/logout" element={<Logout />} />
+            </>
+          )}
+          <Route exact path="*" element={<Page404 />} />
+        </Routes>
+      )}
+    </Suspense>
   )
 }
 
