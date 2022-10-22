2a2aad064cdef47c9729ab93aefe4b214dc4ce65
Micha <mich4el.flores@gmail.com>
2022-10-21 23:30:23 -0300

    Refactor icon display logic in NavBarButton component

--- src/components/NavBarButton.js
+++ src/components/NavBarButton.js
@@ -5,23 +5,18 @@ import { Center, Home, List, Settings } from './icons'
 import PropTypes from 'prop-types'
 
 const NavBarButton = ({ page, text, active, children }) => {
+  const iconPage = {
+    home: <Home color={active ? 'white' : undefined} />,
+    dashboard: <Center color={active ? 'white' : undefined} />,
+    list: <List color={active ? 'white' : undefined} />,
+    settings: <Settings color={active ? 'white' : undefined} />,
+  }
+
   return (
     <Link to={`/${page}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-      <x.div
-        display="flex"
-        alignItems="center"
-        flexDirection="column"
-        gap={{ _: '5px', lg: '15px' }}
-      >
+      <x.div display="flex" alignItems="center" flexDirection="column">
         <x.div p="5px" bg={active && 'blue'} borderRadius={100}>
-          {page === 'home' && <Home color={active ? 'white' : undefined} />}
-          {page === 'dashboard' && (
-            <Center color={active ? 'white' : undefined} />
-          )}
-          {page === 'foods' && <List color={active ? 'white' : undefined} />}
-          {page === 'settings' && (
-            <Settings color={active ? 'white' : undefined} />
-          )}
+          {iconPage[page]}
         </x.div>
         {children ?? <P fontSize={{ _: '12px', lg: '14px' }}>{text}</P>}
       </x.div>
