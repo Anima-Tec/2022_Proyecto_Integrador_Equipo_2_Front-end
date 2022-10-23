95543d162586b9163780da564c70f763fa4f8251
Micha <mich4el.flores@gmail.com>
2022-10-23 13:59:42 -0300

    Review - Create Modal component - Change ModalEditFood name to EditFoodModal - Create SelectUnitMeasurement component - Hotfixes in TextField component - Create edit food inputs validations

---
+++ src/components/SelectUnitMeasurement.js
@@ -0,0 +1,39 @@
+import { Select } from './Select'
+import PropTypes from 'prop-types'
+import { x } from '@xstyled/styled-components'
+import { ArrowDown } from './icons/ArrowDown'
+
+export const SelectUnitMeasurement = ({
+  unitMeasurements,
+  getUnitMeasurementSelected,
+  ...rest
+}) => {
+  const handleOnChange = e => {
+    const unitMeasurementSelected = e.target.value
+
+    getUnitMeasurementSelected(unitMeasurementSelected)
+  }
+
+  return (
+    <x.div w="50%" position="relative">
+      <Select
+        w="100%"
+        name="unitMeasurement"
+        onChange={handleOnChange}
+        {...rest}
+      >
+        {unitMeasurements.map((unitMeasurement, index) => (
+          <x.option key={index} value={unitMeasurement}>
+            {unitMeasurement}
+          </x.option>
+        ))}
+      </Select>
+      <ArrowDown position="absolute" top={0} right={0} bottom={0} m="auto" />
+    </x.div>
+  )
+}
+
+SelectUnitMeasurement.propTypes = {
+  unitMeasurements: PropTypes.array.isRequired,
+  getUnitMeasurementSelected: PropTypes.func.isRequired,
+}
--- src/components/TextField.js
+++ src/components/TextField.js
@@ -2,36 +2,45 @@ import PropTypes from 'prop-types'
 import { x } from '@xstyled/styled-components'
 import { useFormContext } from 'react-hook-form'
 
-export function TextField({ label, name, ...rest }) {
+export function TextField({ isDefault = true, label, name, ...rest }) {
   const {
     register,
     formState: { errors },
   } = useFormContext()
 
   return (
-    <x.div display="flex" flexDirection="column">
-      <x.label>{label}</x.label>
-      <x.input
-        outline="none"
-        borderBottom={1}
-        color={{ placeholder: errors[name] && 'warning' }}
-        borderColor={errors[name] ? 'warning' : 'black'}
-        pt="24px"
-        pb="6px"
-        mb={errors[name] && 2}
-        {...register(name)}
-        {...rest}
-      />
-      {errors[name] && (
-        <x.small color="warning">{errors[name].message}</x.small>
-      )}
+    <x.div
+      display="flex"
+      flexDirection={isDefault && 'column'}
+      gap={!isDefault && '64px'}
+    >
+      <x.label w={!isDefault && '50%'}>{label}</x.label>
+      <x.div w={!isDefault && '50%'} display="flex" flexDirection="column">
+        <x.input
+          w="100%"
+          outline="none"
+          borderBottom={1}
+          color={{
+            _: errors[name] && 'warning',
+            placeholder: errors[name] && 'warning',
+          }}
+          borderColor={errors[name] ? 'warning' : 'black'}
+          pt={isDefault && '24px'}
+          pb={'6px'}
+          mb={errors[name] && 2}
+          {...register(name)}
+          {...rest}
+        />
+        {errors[name] && (
+          <x.small color="warning">{errors[name].message}</x.small>
+        )}
+      </x.div>
     </x.div>
   )
 }
 
 TextField.propTypes = {
+  isDefault: PropTypes.bool,
   label: PropTypes.string,
-  register: PropTypes.any,
-  name: PropTypes.string,
-  errors: PropTypes.any,
+  name: PropTypes.string.isRequired,
 }
